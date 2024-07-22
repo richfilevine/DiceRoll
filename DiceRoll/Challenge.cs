9076f0a423af533d3e1770ae9f81ca7ec44248fe
richfilevine <117759161+richfilevine@users.noreply.github.com>
2024-07-22 16:21:11 +0000

    first commit

---
+++ DiceRoll/Challenge.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace DiceRoll;
+
+public class Challenge
+{
+    public Challenge() { }
+
+    public int GetSum(List<int> values) {
+        // var sum = 0;
+        // foreach (var value in values) {
+        //     sum += value;
+        // }
+        // return sum;
+        return -1;
+    }
+
+    public int GetSumIfThreeMatch(List<int> values) {
+        // var dict = new Dictionary<int, int>();
+        // var sum = 0;
+        // var hasThree = false;
+        // foreach (var value in values) {
+        //     sum += value;
+        //     if (dict.ContainsKey(value)) { dict[value]++; }
+        //     else { dict[value] = 1;}
+        //     if (dict[value] == 3) hasThree = true;
+        // }
+        // return hasThree ? sum : 0;
+        return -1;
+    }
+}
